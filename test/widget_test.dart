fe0abacd45cd24fecc9f06cc53313b740fa3d4bc
Alexandre Reed <ch.alexandre.reed@gmail.com>
2019-10-28 16:47:02 +0100

    mise en place des assets

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,6 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:quizz_app/widgets/my_app.dart';
 
 import 'package:quizz_app/main.dart';
 
